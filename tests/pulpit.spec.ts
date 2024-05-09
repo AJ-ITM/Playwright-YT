e7034c6749b9821eea9c28eae35096e604950754
AJ-ITM <andrzej.jankowski@itmagination.com>
2024-05-09 16:25:46 +0200

    new tests added

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -45,4 +45,23 @@ test.describe('Pulpit tests', () => {
         // Assert
         await expect(page.locator('#show_messages')).toHaveText(`Doładowanie wykonane! ${amountOfMoney},00PLN na numer ${phoneNumber}`);
     });
+
+    test('correct balance after successful mobile top-up', async ({ page }) => {
+        // Arrange
+        const phoneNumber = '500 xxx xxx';
+        const amountOfMoney = '50';
+        const expectedMessage = `Doładowanie wykonane! ${amountOfMoney},00PLN na numer ${phoneNumber}`;
+        const initialBalance = await page.locator('#money_value').innerText();
+        const expectedBalance = Number(initialBalance) - Number(amountOfMoney);
+
+        // Act
+        await page.locator('#widget_1_topup_receiver').selectOption(phoneNumber);
+        await page.locator('#widget_1_topup_amount').fill(amountOfMoney);
+        await page.locator('#widget_1_topup_agreement').click();
+        await page.getByRole('button', { name: 'doładuj telefon' }).click();
+        await page.getByTestId('close-button').click();
+
+        // Assert
+        await expect(page.locator('#money_value')).toHaveText(`${expectedBalance}`);
+    });
 });
